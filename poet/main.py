c374cd5d9388fb685a909d91b512a7beeb91b9f6
zserinz <serinn1023@gmail.com>
2023-08-26 16:16:50 +0900

    feat: dotenv 모듈 삭제 및 requirements 추가

--- poet/main.py
+++ poet/main.py
@@ -1,6 +1,5 @@
-from dotenv import load_dotenv
-load_dotenv()
-
+# from dotenv import load_dotenv
+# load_dotenv()
 from langchain.chat_models import ChatOpenAI
 import streamlit as st
 
@@ -8,7 +7,7 @@ chat_model = ChatOpenAI()
 
 st.title('인공지능 시인')
 
-content = st.text_input('시의 주제를 제시해 주세요.')\
+content = st.text_input('시의 주제를 제시해 주세요.')
 
 if st.button('시 작성 요청하기'):
     with st.spinner('시 작성 중...'):
